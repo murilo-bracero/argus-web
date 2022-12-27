19cae00d7e25325765d1d016521c6fcd094c4343
Murilo Bracero <murilobracero@gmail.com>
2022-12-27 18:41:25 -0300

    removed authentication service

--- auth-api/src/main/kotlin/br/com/argus/authapi/dto/ErrorResponseDTO.kt
+++
@@ -1,6 +0,0 @@
-package br.com.argus.authapi.dto
-
-data class ErrorResponseDTO(
-    val code: String = "",
-    val message: String = ""
-)
--- auth-api/src/main/kotlin/br/com/argus/authapi/model/Tokens.kt
+++
@@ -1,6 +0,0 @@
-package br.com.argus.authapi.model
-
-data class Tokens(
-    val accessToken: String,
-    val refreshToken: String
-)
